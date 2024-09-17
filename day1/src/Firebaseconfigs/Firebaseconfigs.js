c6c517929603af37c55e9e71ec87c12c71a3b970
CharviManthena <119869600+CharviManthena@users.noreply.github.com>
2024-09-17 12:18:25 +0530

    Integrating Firebase

---
+++ day1/src/Firebaseconfigs/Firebaseconfigs.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import {getAuth} from  "firebase/auth";
+import {getFirestore} from  "firebase/firestore";
+import {getStorage} from   "firebase/storage";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA2mZgGrGIV_31bnHT8hTSQvX56MgIS5to",
+  authDomain: "ecommerce-9bdbb.firebaseapp.com",
+  projectId: "ecommerce-9bdbb",
+  storageBucket: "ecommerce-9bdbb.appspot.com",
+  messagingSenderId: "1023114788198",
+  appId: "1:1023114788198:web:1505cc5d03dda8e978dca7"
+};
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export  const db = getFirestore(app);
+export  const storage = getStorage(app);
+
+
