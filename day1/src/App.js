c2178c65f8cc7a3298ba7ba391eddd95dfa4334c
CharviManthena <119869600+CharviManthena@users.noreply.github.com>
2024-09-17 12:24:15 +0530

    ..

--- day1/src/App.js
+++ day1/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import PgFOF from './components/PgFOF';
+import Navbar from './components/Navbar';
+import cart from './components/cart';
+import profile from './components/profile';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/home" element={<Home />} />
+      <Route exact path="/signup" element={<Signup />} />
+      <Route exact path="/login" element={<Login />} />
+      <Route exact path="/cart" element={<cart />} />
+      <Route exact path="/userprofile" element={<profile/>} />
+      <Route path="*" element={<PgFOF />} />
+    </Routes>
+    </BrowserRouter>
   );
 }
 
